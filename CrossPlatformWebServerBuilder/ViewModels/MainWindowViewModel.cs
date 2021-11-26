e88872e797aa9683a464de5b6cf7f93f24f3c94c
an-prata <evanrileyoverman@gmail.com>
2021-11-26 11:07:15 -0800

    Initial commit

---
+++ CrossPlatformWebServerBuilder/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Evan Overman (https://github.com/an-prata)
+// CrossPlatformWebServerBuilder ()
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossPlatformWebServerBuilder.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string TextBoxScript_Text => "Welcome to Avalonia!";
+    }
+}
---
+++ CrossPlatformWebServerBuilder/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace CrossPlatformWebServerBuilder.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
